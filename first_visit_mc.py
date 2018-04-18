886cdfdc43646fcbe988298f56a0b3bcccdbd5dd
chuk <chukmunnlee@gmail.com>
2018-04-18 10:23:16 +0800

    wireframe working

--- first_visit_mc.py
+++ first_visit_mc.py
@@ -3,21 +3,23 @@ import pickle as pickle
 from env import Easy21
 
 import numpy as np
+from scipy.interpolate import griddata
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 
 easy21 = Easy21()
 
-EPISODE = 20000
+EPISODE = 10000
 
 episodes = pickle.load(open(sys.argv[1], 'rb'))
 
 # ((dealer, player), reward)
 print('Episodes: %d' %len(episodes))
+EPISODE = len(episodes)
 
 reward = lambda s: s[1]
 value = lambda s, val: val[s] if s in val else 0
-count = lambda s, cnt: cnt[s] if s in cnt else 1
+count = lambda s, cnt: cnt[s] if s in cnt else 0
 
 val = {}
 cnt = {}
@@ -34,27 +36,37 @@ for i, ep in enumerate(episodes[:EPISODE]):
       val[st] = curr[st] + value(st, val)
       cnt[st] = count(st, cnt) + 1
 
+# ((dealer, player), reward)
 value_pi = [ (r[0], r[1]/cnt[r[0]]) for r in val.items() ]
 
-player = [] #X
-dealer = [] #Y
-reward = [] #Z
-for r in value_pi:
-   dealer.append(r[0][0])
-   player.append(r[0][1])
-   reward.append(r[1])
+x = [ r[0][0] for r in value_pi ]
+y = [ r[0][1] for r in value_pi]
+z = [ r[1] for r in value_pi ]
 
-X, Y = np.meshgrid(player, dealer)
-print('X = ', X)
-print('Y = ', Y)
+xi = np.linspace(0, 10, 20)
+yi = np.linspace(0, 21, 42)
+zi = griddata((x, y), z, (xi[None, :], yi[:, None]), method='cubic')
+
+#print('value_pi = ', value_pi)
+#print('\tx = ', x, len(x))
+#print('\ty = ', y, len(y))
+#print('\tz = ', z, len(z))
+#print('\txi = ', xi, len(xi))
+#print('\tyi = ', yi, len(yi))
+#print('\tzi = ', zi, len(zi))
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.set_xlabel('X = player')
-ax.set_ylabel('Y = dealer')
+
+ax.set_xlabel('X = dealer')
+ax.set_ylabel('Y = player')
 ax.set_zlabel('Z = REWARD')
 
+X, Y = np.meshgrid(xi, yi)
+
 #https://stackoverflow.com/questions/18923502/plotting-contour-and-wireframe-plots-with-matplotlib
-ax.plot_wireframe(X, Y, reward)
+ax.plot_wireframe(X, Y, zi, rstride = 1, cstride = 1)
+
+plt.title('Easy21 for %d episodes' %EPISODE)
 
 plt.show()
