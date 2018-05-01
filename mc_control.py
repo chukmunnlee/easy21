277b08b80fae745382f7d649bf53c5bb66f3fba9
chuk <chukmunnlee@gmail.com>
2018-05-01 13:19:19 +0800

    mc control - v* plot

--- mc_control.py
+++ mc_control.py
@@ -1,5 +1,6 @@
 import sys
 import time
+from tqdm import tqdm
 
 import pickle as pickle
 import numpy as np
@@ -82,15 +83,16 @@ win = 0;
 #start
 easy21 = Easy21()
 
-for i in range(EPISODE):
-   print('Episode: %d' %i)
+for i in tqdm(range(EPISODE)):
    episode = generate_episode()
+   w = episode[-1][2] > 0
+   #print('Episode: %d, len=%d, win=%s' %(i, len(episode), str(w)))
    #print('episode = ', episode)
 
    #for first visit
    visited = {}
 
-   if (episode[-1][2] > 0):
+   if w:
       win += 1
    else:
       lose += 1
@@ -115,12 +117,13 @@ for i in range(EPISODE):
       #print('e = ', episode[idx:], ', g = ', g)
 
 #save the q_value
-pickle.dump(q_value, open('q_value_%d_%d.pickle' %(EPISODE, round(time.time())), 'wb'))
+pickle.dump(q_value, open('episodes/q_value_%d_%d.pickle' %(EPISODE, round(time.time())), 'wb'))
 
 dealer = [] # 0 - 10 X
 player = [] # 0 - 21 Y
 hit_qval = [] # -1 - 1 Z
 stick_qval = [] # -1 - 1 Z
+arg_max = [] # -1 - 1 Z
 # { (dealer, player_sum): (HIT, STICK), ... }
 for st in q_value:
    a = q_value[st]
@@ -128,6 +131,7 @@ for st in q_value:
    player.append(st[1])
    hit_qval.append(a[Easy21.HIT])
    stick_qval.append(a[Easy21.STICK])
+   arg_max.append(max(a))
 
 print('state count = ', len(state_count))
 print('state action count = ', len(state_action_count))
@@ -141,27 +145,35 @@ print('win = %d, lose = %d' %(win, lose))
 xi = np.linspace(0, 10, 22);
 yi = np.linspace(0, 21, 42);
 zi_hit = griddata((dealer, player), hit_qval, (xi[None, :], yi[:, None]), method='cubic')
-
 zi_stick = griddata((dealer, player), stick_qval, (xi[None, :], yi[:, None]), method='cubic')
+zi_argmax = griddata((dealer, player), arg_max, (xi[None, :], yi[:, None]), method='cubic')
 
 X, Y = np.meshgrid(xi, yi)
 
 fig = plt.figure()
 
-ax = fig.add_subplot(121, projection='3d')
+ax = fig.add_subplot(221, projection='3d')
 ax.set_xlabel('X = dealer')
 ax.set_ylabel('Y = player')
 ax.set_zlabel('Z = hit')
 ax.set_title('HIT: episode: %d' %EPISODE)
 
-ax.plot_wireframe(X, Y, zi_hit, rstride=1, cstride=1, color="blue")
+ax.plot_wireframe(X, Y, zi_hit, rstride=1, cstride=1, color='blue')
 
-ay = fig.add_subplot(122, projection='3d')
+ay = fig.add_subplot(222, projection='3d')
 ay.set_xlabel('X = dealer')
 ay.set_ylabel('Y = player')
 ay.set_zlabel('Z = stick')
 ay.set_title('STICK: episode: %d' %EPISODE)
 
-ay.plot_wireframe(X, Y, zi_stick, rstride=1, cstride=1, color="red")
+ay.plot_wireframe(X, Y, zi_stick, rstride=1, cstride=1, color='red')
+
+az = fig.add_subplot(223, projection='3d')
+az.set_xlabel('X = dealer')
+az.set_ylabel('Y = player')
+az.set_zlabel('Z = argmax Q(s, a)')
+az.set_title('V*(s): episode: %d' %EPISODE)
+
+az.plot_wireframe(X, Y, zi_argmax, rstride=1, cstride=1, color='green')
 
 plt.show()
